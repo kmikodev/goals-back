01fe6e6b88a5d2e6817fc03263380320765a21bc
Jaime Cardona Villegas <jcardonavillegas@gmail.com>
2024-12-28 02:35:48 +0100

    first commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
-const serviceAccount =  JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+dotenv.config();
+
+const serviceAccount =  JSON.parse(atob(process.env.FIREBASE_SERVICE_ACCOUNT));
 
 const app = initializeApp({
   credential: cert(serviceAccount)
