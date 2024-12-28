d8b21a66714dde1684091a1ae636fd2666661d7b
Jaime Cardona Villegas <jcardonavillegas@gmail.com>
2024-12-28 03:06:54 +0100

    WIP

--- src/services/goals/goalPlanGenerator.js
+++ src/services/goals/goalPlanGenerator.js
@@ -4,7 +4,7 @@ import { SYSTEM_PROMPTS } from '../../utils/prompts.js';
 export const generateGoalPlan = async (userPrompt) => {
   try {
     const response = await openai.chat.completions.create({
-    model: 'gpt-4',
+    model: 'gpt-4-turbo',
       messages: [
         {
           role: 'system',
